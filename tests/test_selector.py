6ae95baec115d0b2a7f93c1a89dfa13098c649df
Sam Vilain <sam@vilain.net>
2014-03-27 11:34:21 -0700

    Move 'testclasses' into the single test which used them
    These definitions were a bit 'foo', and only used by one class so move
    them into that file.

--- tests/test_selector.py
+++ tests/test_selector.py
@@ -1,14 +1,50 @@
 from __future__ import absolute_import
 
+from datetime import datetime
 import unittest
 
 from normalize import FieldSelector
 from normalize import FieldSelectorException
+from normalize import JsonCollectionProperty
+from normalize import JsonProperty
+from normalize import JsonRecord
 from normalize import Property
 from normalize import Record
+from normalize import Record
+from normalize import RecordList
 from normalize.property.coll import ListProperty
 from normalize.selector import MultiFieldSelector
-from testclasses import MockChildRecord, MockJsonRecord
+
+
+class MockChildRecord(JsonRecord):
+    name = JsonProperty()
+
+
+class MockDelegateJsonRecord(JsonRecord):
+    other = JsonProperty()
+
+
+class MockJsonRecord(JsonRecord):
+    name = JsonProperty()
+    age = JsonProperty(isa=int)
+    seen = JsonProperty(
+        json_name='last_seen', isa=datetime,
+        coerce=lambda x: datetime.strptime(x, '%Y-%m-%dT%H:%M:%S'),
+    )
+    children = JsonCollectionProperty(of=MockChildRecord)
+
+
+class MockExtraneousJsonRecord(JsonRecord):
+    count = JsonProperty(isa=int)
+    last_updated = JsonProperty(
+        isa=datetime,
+        coerce=lambda x: datetime.strptime(x, '%Y-%m-%dT%H:%M:%S'),
+        extraneous=False,
+    )
+
+
+class MockRecordList(RecordList):
+    itemtype = MockExtraneousJsonRecord
 
 
 class MockComplexJsonRecord(MockJsonRecord):
