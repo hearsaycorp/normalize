eacdff4d9bda8aec5603f0be6316fb06b932a59d
Sam Vilain <sam@vilain.net>
2014-04-03 10:11:54 -0700

    Bump version to 0.1.0
    Documentation probably deserves moving off the 0.0 version series.

--- doc/conf.py
+++ doc/conf.py
@@ -22,7 +22,7 @@ project = u'normalize'
 copyright = u'2014, Sam Vilain, Hearsay Social'
 
 version = '0.1'
-release = '0.0.4'
+release = '0.1.0'
 
 exclude_patterns = ['sphinx-build']
 pygments_style = 'sphinx'
