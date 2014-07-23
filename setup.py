963199458279befa1f3fa5a83375c3de45f77ec4
Sam Vilain <sam@vilain.net>
2014-07-23 16:42:44 -0700

    Release engineering for 0.5.0
    Let's try and put the README into the long description :)

--- setup.py
+++ setup.py
@@ -1,16 +1,28 @@
 from setuptools import find_packages, setup
 
 
+try:
+    with open("README.rst", "ro") as readme:
+        lines = []
+        for line in readme:
+            lines.append(line)
+            if "...and much more" in line:
+                break
+        long_description = "".join(lines)
+except:
+    long_description = """
+This module lets you declare classes and object properties, and then
+get support for marshaling to and from JSON data.  You can also compare
+objects to see if they have changed in meaningful ways.
+"""
+
+
 setup(
     author='Hearsay Labs, Inc',
     author_email='svilain@hearsaylabs.com',
     description="Declarative Python meta-model system and visitor utilities",
     license='MIT',
-    long_description="""
-This module lets you declare classes and object properties, and then
-get support for marshaling to and from JSON data.  You can also compare
-objects to see if they have changed in meaningful ways.
-""",
+    long_description=long_description,
     name='normalize',
     packages=find_packages(),
     install_requires=('richenum>=1.0.0',),
