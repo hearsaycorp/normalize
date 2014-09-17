4254de878fbdda28a8eea5f559c69a3bb4338188
Sam Vilain <sam@vilain.net>
2014-09-17 15:50:22 -0700

    Bump version for release

--- setup.py
+++ setup.py
@@ -27,6 +27,6 @@ setup(
     packages=find_packages(),
     install_requires=('richenum>=1.0.0',),
     test_suite="run_tests",
-    version='0.5.5',
+    version='0.6.0',
     url="http://hearsaycorp.github.io/normalize",
 )
