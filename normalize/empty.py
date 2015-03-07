74592543704efd4d6a9224e96b1df40915c2aa88
Sam Vilain <sam@vilain.net>
2015-03-06 17:12:21 -0800

    Empty: Document class a bit

--- normalize/empty.py
+++ normalize/empty.py
@@ -6,6 +6,7 @@ EMPTY_VALS = dict()
         
 
 def placeholder(type_):
+    """Returns the EmptyVal instance for the given type"""
     typetuple = type_ if isinstance(type_, tuple) else (type_,) 
     if any in typetuple:
         typetuple = any
@@ -15,6 +16,8 @@ def placeholder(type_):
 
 
 def itertypes(iterable):
+    """Iterates over an iterable containing either type objects or tuples of
+    type objects and yields once for every type object found."""
     seen = set()
     for entry in iterable:
         if isinstance(entry, tuple):
@@ -29,6 +32,24 @@ def itertypes(iterable):
 
 
 class EmptyVal(object):
+    """Empty Value placeholder for a given tuple of types.  This object's
+    ``__nonzero__`` always returns ``False``, and it knows how to infer types
+    as its attributes are accessed and/or lists are de-referenced: so long as
+    there are type hints in the places normalize puts them.
+
+    Each set of types gets a single, persistent ``EmptyVal`` object.  The
+    special ``EmptyVal(any)`` is used when types cannot be inferred, and never
+    raises any errors: it just returns itself when invoked etc.
+
+    If types can be inferred, then exceptions are raised when invalid
+    attributes are accessed, or a type which does not implement ``__getitem__``
+    is dereferenced, or a type which does not supply ``__call__`` is invoked.
+
+    This function will raise exceptions badly when you try to access variables
+    which are only available in a subclass of the declared class on the type
+    hint, or when a property attribute is set by a method (eg, ``__init__``)
+    rather than declared on the class.
+    """
     def __init__(self, typetuple):
         self._typetuple = typetuple
         self._attrs = {}
