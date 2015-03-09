1a237ae78d5eb9bdfad8814a76b0ed17d6a807ee
Sam Vilain <sam@vilain.net>
2015-03-09 09:41:01 -0700

    Bump doc version, too

--- doc/conf.py
+++ doc/conf.py
@@ -22,8 +22,8 @@ master_doc = 'index'
 project = u'normalize'
 copyright = u'2014, Sam Vilain, Hearsay Social'
 
-version = '0.7'
-release = '0.7.2'
+version = '0.8'
+release = '0.8.0'
 
 exclude_patterns = ['sphinx-build']
 pygments_style = 'sphinx'
